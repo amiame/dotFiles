30f8bb00e60242c39747e62238c2db16ea0def5d
Amir Fikri <amir_fikri@egxp.io>
2023-01-30 14:41:39 +0900

    Added nvim and tmux-sessionizer dotfiles

---
+++ .config/nvim/lua/amir/set.lua
@@ -0,0 +1,38 @@
+vim.opt.guicursor = ""
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.listchars = { space = ' ', tab = '•›', trail = '~', nbsp = '%', extends = '>', precedes = '<' }
+
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim.undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = false
+
+vim.opt.scrolloff = 99
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "120"
+
+vim.g.mapleader = " "
