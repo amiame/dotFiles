204a07a9e7a09905f1aa435ff18b8f67ef8b4096
amiame <amiame2008@gmail.com>
2023-02-08 04:49:26 +0900

    Fixed goenv config file

--- .config/fish/conf.d/goenv.fish
+++ .config/fish/conf.d/goenv.fish
@@ -1,5 +1,5 @@
-set GOENV_ROOT $HOME/.goenv
+set GOENV_ROOT /home/linuxbrew/.linuxbrew
 set PATH $GOENV_ROOT/bin $PATH
-status --is-interactive; and source (/home/linuxbrew/.linuxbrew/bin/goenv init -|psub)
+status --is-interactive; and source (goenv init -|psub)
 set PATH $GOROOT/bin $PATH
 set PATH $PATH GOPATH/bin
