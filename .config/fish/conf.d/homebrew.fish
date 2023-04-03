c9cce812d750f4c1c103650d0340dfffdc844930
amiame <amiame2008@gmail.com>
2023-04-03 14:57:11 +0900

    Added fish functions for MacOS

--- .config/fish/conf.d/homebrew.fish
+++ .config/fish/conf.d/homebrew.fish
@@ -1,2 +1,6 @@
+# For Ubuntu
 # Set PATH, MANPATH, etc., for Homebrew.
 status --is-interactive; and source (/home/linuxbrew/.linuxbrew/bin/brew shellenv|psub)
+
+# For MacOS
+status --is-interactive; and source (/usr/local/bin/brew shellenv|psub)
