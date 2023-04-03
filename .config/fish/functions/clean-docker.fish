0d971ea29ff1285879bea325d108a8e701e87f0e
amiame <amiame2008@gmail.com>
2023-04-03 16:01:16 +0900

    Updated file

--- .config/fish/functions/clean-docker.fish
+++ .config/fish/functions/clean-docker.fish
@@ -1,3 +1,8 @@
 function clean-docker
-    docker rm (docker container list --format '{{ .ID }}'); docker container prune -f; docker image prune -f; docker volume prune -f; docker system prune -f
+    docker stop (docker container list --format '{{ .ID }}')
+    docker rm (docker container list --format '{{ .ID }}')
+    docker container prune -f
+    docker image prune -f
+    docker volume prune -f
+    docker system prune -f
 end
