7d10ef593b7291c3cd3a38b1ab2f8ac9388d5881
amiame <amiame2008@gmail.com>
2023-07-28 14:10:25 +0900

    Fix broken window navigating key bindings

--- .config/nvim/lua/amir/remap.lua
+++ .config/nvim/lua/amir/remap.lua
@@ -39,9 +39,11 @@ vim.keymap.set("n", "<leader>f", function()
     vim.lsp.buf.format()
 end)
 
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>", "<cmd>lprev<CR>zz")
-
 vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
+
+vim.keymap.set("n", "-", ":Ex<CR>")
+
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
